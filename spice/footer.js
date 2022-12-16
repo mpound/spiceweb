b8373136618250fa1ca58c2b22a010f412adfeb5
Marc Pound <22331890+mpound@users.noreply.github.com>
2022-12-16 10:29:55 -0500

    initial copy of gsfc spice website

---
+++ spice/footer.js
@@ -0,0 +1,14 @@
+document.write('\
+					<footer class="wrapper style1 invert align-center">\
+						<div class="inner">\
+							<ul class="icons">\
+								<li><a href="https://twitter.com/NASA" class="icon brands style2 fa-twitter"><span class="label">Twitter</span></a></li>\
+								<li><a href="https://www.facebook.com/NASA" class="icon brands style2 fa-facebook-f"><span class="label">Facebook</span></a></li>\
+								<li><a href="https://www.instagram.com/nasa" class="icon brands style2 fa-instagram"><span class="label">Instagram</span></a></li>\
+								<li><a href="https://www.linkedin.com/company/nasa/" class="icon brands style2 fa-linkedin-in"><span class="label">LinkedIn</span></a></li>\
+								<li><a href="#" class="icon style2 fa-envelope"><span class="label">Email</span></a></li>\
+							</ul>\
+							<p>NASA Official: <a href="&#109;&#097;&#105;&#108;&#116;&#111;&#058;&#100;&#097;&#118;&#105;&#100;&#046;&#116;&#046;&#108;&#101;&#105;&#115;&#097;&#119;&#105;&#116;&#122;&#064;&#110;&#097;&#115;&#097;&#046;&#103;&#111;&#118;">David Leisawitz</a> | Curator: <a href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#106;&#111;&#104;&#110;&#46;&#100;&#46;&#109;&#121;&#101;&#114;&#115;&#64;&#110;&#97;&#115;&#97;&#46;&#103;&#111;&#118;">J.D. Myers</a> | Designer: Petr Pokorny | Last updated: Today</p>\
+						</div>\
+					</footer>\
+					');
---
+++ spice/nav-bar.js
@@ -0,0 +1,24 @@
+document.write('\
+	 			<header id="header">\
+					<!-- <h1><a href="index.html">Alpha</a> by HTML5 UP</h1> -->\
+					<nav id="nav">\
+						<ul>\
+							<li><a href="index.html">SPIRIT</a></li>\
+							<li>\
+								<a href="teams.html" class="icon solid fa-angle-down">Team Profiles</a>\
+								<ul>\
+									<li><a href="teams.html#leader">Leadership</a></li>\
+									<li><a href="teams.html#galactic">Extragalactic Science</a></li>\
+									<li><a href="teams.html#proto">Protoplanetary</a></li>\
+									<li><a href="teams.html#debris">Debris Disks</a></li>\
+									<li><a href="teams.html#instrumentation">Instrumentation</a></li>\
+								</ul>\
+							</li>\
+							<li><a href="science.html">Science</a></li>\
+							<!-- <li><a href="#" class="button">Sign Up</a></li> -->\
+						</ul>\
+					</nav>\
+				</header>\
+');		
+
+					
